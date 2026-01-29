a53cb2f13fcf74f8b20a9965932475476e474c71
bebrochkaa4 <samirakhakim@gmail.com>
2026-01-29 17:39:22 +0300

    Add files via upload
    add js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-function addContact() {
-    const vcard = `BEGIN:VCARD
-VERSION:3.0
-FN:Булат Хакимов
-TEL:+79274272524
-END:VCARD`;
+const addContactBtn = document.getElementById("addContact");
+const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-    const blob = new Blob([vcard], { type: "text/vcard" });
-    const url = URL.createObjectURL(blob);
-
-    window.location.href = url;
+if (/iPad|iPhone|iPod/.test(userAgent)) {
+  addContactBtn.href = "contact.vcf";
+} else if (/Android/.test(userAgent)) {
+  addContactBtn.href = "tel:+79274272524";
+} else {
+  addContactBtn.href = "contact.vcf";
 }
